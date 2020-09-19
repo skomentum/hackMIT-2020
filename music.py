46be83ac235dc4f44e1a16c1902fb120efd6da48
katherineaubert <kaubert@mymail.mines.edu>
2020-09-19 16:41:32 -0600

    Saves generated sound to an mp3 file

--- music.py
+++ music.py
@@ -7,8 +7,8 @@ import ffmpeg
 # Creates an empty AudioSegment
 result = AudioSegment.silent(duration=0)
 
-# Loop over 0-14
-for n in range(15):
+# Loop over 1-13 because this is the range that sounds pleasing to hear
+for n in range(1, 13):
     # Generate a sine tone with frequency 200 * n
     gen = Sine(200 * n)
     # AudioSegment with duration 200ms, gain -3
@@ -19,3 +19,5 @@ for n in range(15):
     result += sine
 # Play the result
 play(result)
+#save the result as an mp3 file
+result.export("test.mp3", format="mp3")
