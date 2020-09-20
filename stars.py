3ae616f6182efbe7ac475722b0f387b6a6a3d63e
cadecrandall <ccrandall@mymail.mines.edu>
2020-09-19 22:47:50 -0600

    help

--- stars.py
+++ stars.py
@@ -24,9 +24,13 @@ SOFTWARE.
 
 import numpy as np
 import pandas as pd
+import skyfield
 from matplotlib import pyplot as plt
 from skyfield import almanac, api, data
 import astropy
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from skyfield.starlib import Star
 
 
 def get_stars(zip_code, date_utc):
@@ -36,38 +40,62 @@ def get_stars(zip_code, date_utc):
     :return:          returns a df of the bright stars
     """
     load = api.Loader('./data')     # put the data here
-    manhattan_beach = api.Topos('33.881519 N', '118.388177 W')      # example location
+    manhattan_beach = api.Topos('33.881519 S', '118.388177 W')      # example location
+
+    ephemeris = load('de421.bsp')   # download JPL ephemeris
+    earth = ephemeris['earth']
 
     ts = load.timescale()
-    ephemeris = load('de421.bsp')   # download JPL ephemeris
+    t = ts.utc(2001, 1, 1)            # date is today
 
     with load.open(data.hipparcos.URL) as f:
         df = data.hipparcos.load_dataframe(f)
 
-    earth = ephemeris['earth']
-    t = ts.now()            # date is today
+    barnards_star = Star.from_dataframe(df.loc[87937])
 
-    bright = df[df['magnitude'] <= 5.5]     # don't know what this does tbh
+    astrometric = earth.at(t).observe(barnards_star)
+    ra, dec, distance = astrometric.radec()
+    print(ra)
+    print(dec)
+
+    bright = df[df['magnitude'] <= 5.5]                 # Prevent apparent magnitude from being greater than 5.5
     bright_stars = api.Star.from_dataframe(bright)
 
     t = ts.now()
     astrometric = earth.at(t).observe(bright_stars)
     ra, dec, distance = astrometric.radec()
 
-    observer = earth + manhattan_beach
+    print(ra.describe())
+    print(dec)
+    print(distance)
 
     return transform_stars(bright)
 
 
 def transform_stars(df):
     """
+    Transform the dataset
     :param df:  dataframe containing bright stars
     :return:    returns a two-dimensional list
                     [magnitude, ra_degrees, dec_degrees]
     """
     # Remove additional data from the star file
     df = df.drop(columns=['parallax_mas', 'ra_mas_per_year', 'dec_mas_per_year', 'ra_hours', 'epoch_year'])
-    out = []
+
+    out = []    # list with sorted cartesian coords that will be returned
     for row_label, row in df.iterrows():
-        out.append([row[0], row[1], row[2]])
-    return out
+        n = 0
+        inserted = 0
+        while n < len(out) and inserted == 0:
+            if row[1] <= out[n][1]:
+                # shift items after index n and insert curr at n
+                out.insert(n, [row[0], row[1], row[2]])
+                inserted = 1
+            else:
+                n += 1
+
+        if inserted == 0:               # curr has not been inserted in out yet - will be inserted at the end
+            out.append([row[0], row[1], row[2]])
+
+    print(out)
+    return ""
